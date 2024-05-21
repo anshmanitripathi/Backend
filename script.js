0b6ac234821d8600b6251e4b744aeec86244e2f7
Ansh Mani Tripathi <143960008+anshmanitripathi@users.noreply.github.com>
2024-05-21 15:21:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// let n=5;
+
+// for(let i=0;i<n;i++){
+//     console.log("hello", i);
+// }
+
+// console.log(process.argv);
+
+// for(let i=2; i<process.argv.length;i++){
+//     console.log("hello to", process.argv[i]);
+// }
+
+
+const math = require("./math");
+
+console.log(math.sum(7,9));
+console.log(math.mul(7,9));
+
+
+
