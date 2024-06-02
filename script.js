bc6ecc2cea80807a1e59061d2d072722b7befd70
Ansh Mani Tripathi <143960008+anshmanitripathi@users.noreply.github.com>
2024-06-03 03:30:01 +0530

    Express

--- script.js
+++ script.js
@@ -11,15 +11,32 @@
 // }
 
 
-const math = require("./math");
+// const math = require("./math");
 
-console.log(math.sum(7,9));
-console.log(math.mul(7,9));
+// console.log(math.sum(7,9));
+// console.log(math.mul(7,9));
 
-const fruits = require("./fruits");
+// const fruits = require("./fruits");
 
-console.log(fruits);
-console.log(fruits[2].color);
+// console.log(fruits);
+// console.log(fruits[2].color);
 
+// const figlet = require("figlet");
+
+// figlet("Ansh Mani", function (err, data) {
+//     if (err) {
+//       console.log("Something went wrong...");
+//       console.dir(err);
+//       return;
+//     }
+//     console.log(data);
+//   });
+
+
+import { generate } from "random-words";
+import { sum } from "./math.js";
+console.log(generate());
+
+console.log(sum(6,5));
 
 
